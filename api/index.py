d01ee7dd50a6ab6b6d36dbc98027ed3612f7c1ae
Paulo Silva <paulo@pagus.com.br>
2021-03-29 18:21:36 -0300

    back to basics

--- api/index.py
+++ api/index.py
@@ -1,14 +1,16 @@
-from flask import Flask, jsonify
+from http.server import BaseHTTPRequestHandler
 from models import category
-
-app = Flask(__name__)
-
-
-@app.route('/categorize/<text>')
-def categorize(text):
-    classification = category.classify(text)
-    return jsonify(classification)
+from urlparse import urlparse
 
 
-if __name__ == '__main__':
-    app.run()
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        query = urlparse(self.path).query
+        query_components = dict(qc.split("=") for qc in query.split("&"))
+        text = query_components["text"]
+        category.classify(text)
+        self.send_response(200, text)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        return
